341d29bc5d6cdd073bbf5707cec028f3a2ffafca
sageetemple <sageetemple@gmail.com>
2016-09-08 09:34:24 -0700

    Lesson_02

---
+++ Lab_02.py
@@ -0,0 +1,22 @@
+a = 5
+b = 8
+print("8 multiplied by 5 is", a * b)
+print("\n")
+
+
+a = "Sage Templeton"
+b = "345 Carmel Mountain Dr"
+c = "SD, CA"
+d = "92130"
+
+print(a)
+print(b)
+print(c+ " ", d)
+print("\n")
+print(" ", a + "\n", b + "\n", c + " ", d)
+print("\n")
+w = 7
+l = 8
+h = 6
+
+print("The area of your prism is", (w*l + h*l + h*w)*2)
