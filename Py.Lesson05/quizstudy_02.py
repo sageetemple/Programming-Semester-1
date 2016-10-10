c46b70d58571ef5488bbf34b963d2dabe6264721
sageetemple <sageetemple@gmail.com>
2016-10-10 10:24:59 -0700

    Lesson05

---
+++ Py.Lesson05/quizstudy_02.py
@@ -0,0 +1,17 @@
+num1=67
+num2=89
+def calcSum(one,two):
+    return one+two
+def sumPrint(one,two):
+    print("The sum of", one,"and",two,"is",calcSum(one,t
+item="Burger"
+price=5.67
+def formatreceipt(one,two):
+    print("{:<10}".format(one),"{:.>10.2f}".format(two))
+formatreceipt(item,price)
+
+item1=45.6
+item2=45.2
+def formatinput(one,two):
+    print("The total cost of you order is {:5.2f}".format(one+two))
+formatinput(item1,item2)
