12738f707aa68ebcb78a2f6221f8ecaa63cc9d20
sageetemple <sageetemple@gmail.com>
2016-10-12 09:47:27 -0700

    AddedLesson05

--- Py.Lesson05/gpa.py
+++ Py.Lesson05/gpa.py
@@ -1,8 +1,11 @@
 math=input("Enter your Math grade:")
 science=input("Enter your Science grade:")
-
+history=input("Enter your History grade:")
+compsci=input("Enter your Computer Science grade:")
+english=input("Enter your English grade:")
+language=input("Enter your Language grade:")
+elective=input("Enter your Elective grade:")
 grade=0
-
 def calcPoints(subject):
     global grade
     if subject=="A":
@@ -15,11 +18,22 @@ def calcPoints(subject):
         grade=1.0
     if subject=="F":
         grade=0
-gpa = (calcPoints(math))+ (calcPoints(science))
-print(gpa)
+calcPoints(math)
+mathgrade = grade
+calcPoints(science)
+sciencegrade= grade
+calcPoints(history)
+historygrade=grade
+calcPoints(compsci)
+compscigrade=grade
+calcPoints(english)
+englishgrade=grade
+calcPoints(language)
+languagegrade=grade
+calcPoints(elective)
+electivegrade=grade
+gpa=(mathgrade+sciencegrade+historygrade+compscigrade+englishgrade+languagegrade+electivegrade)/7
+print("Your GPA is", gpa)
 
-history=input("Enter your History grade:")
-compsci=input("Enter your Computer Science grade:")
-english=input("Enter your English grade:")
-language=input("Enter your Language grade:")
-elective=input("Enter your Elective grade:")
+
+
