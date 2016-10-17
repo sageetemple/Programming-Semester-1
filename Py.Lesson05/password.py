a0d7259f32b2898123ea597d855d5f4f56f57926
sageetemple <sageetemple@gmail.com>
2016-10-17 09:39:58 -0700

    UpdatedLesson05

--- Py.Lesson05/password.py
+++ Py.Lesson05/password.py
@@ -5,11 +5,16 @@ def passCheck():
     usercheck= input("Please input your username: ")
     if password==wordcheck and user==usercheck:
         print("You are granted access!!")
-    if password!=wordcheck and user=usercheck:
-        print("Your password is incorrect!)
+    elif password!=wordcheck and user==usercheck:
+        print("Your password is incorrect!")
         passCheck()
-    if password==wordcheck and user!=usercheck:
+    elif password==wordcheck and user!=usercheck:
         print("Your username is incorrect!!")
         passCheck()
+    else:
+        print("Your username and password are incorrect!")
+        passCheck()
+passCheck()
+              
         
               
