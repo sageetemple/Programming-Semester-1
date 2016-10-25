49a2b198943a95b9857a23188272a8be73ac6d0a
sageetemple <sageetemple@gmail.com>
2016-10-25 10:22:16 -0700

    Lesson06.1

--- Py.Lesson06/song.py
+++ Py.Lesson06/song.py
@@ -1,4 +1,18 @@
-def loop():
-    word=input("Please enter your word: ")
-    num=int(input("Please enter your number: "))
-    for  in range(0,
+word1="Na"
+num=4
+output=" "
+def loop(word,num,output):
+    for i in range(0,(num+1)*len(word),len(word)):
+        output=output+ str(word[0:i])+" "
+    print(output)
+loop(word1,num,output)
+loop(word1,num,output)
+word2="Hey"
+num2=3
+loop(word2,num2,output)
+word3="Goodbye!"
+num3=1
+loop(word3,num3,output)
+print("\n \nDo you know the song?")
+
+
