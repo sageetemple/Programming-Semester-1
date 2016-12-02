62abd497ec1911dcc2a0aa827fb7de0f0bb95afd
sageetemple <sageetemple@gmail.com>
2016-12-02 10:24:50 -0800

    Lesson_09

--- Py.Lesson09/composites.py
+++ Py.Lesson09/composites.py
@@ -1,15 +1,12 @@
 nums= [2,6,8,9,10,12,13,15,17,24,55,66,78,77,79]
 def gFactor(number):
-      for z in range(2,number,1):
-            if (number%z)==0:
+      for z in range(2 , number , 1):
+            if (number % z)== 0:
                 return 1
-            else:
-                return 0
-
-
+      return 0
 def removePrimes():
       for i in nums:
-            if gFactor(i)==0:
-                  nums.remove(i)
+            if gFactor(i )== 0:
+                  nums.remove(i )
 removePrimes()
 print(nums)
--- Py.Lesson09/expressionsolver.py
+++ Py.Lesson09/expressionsolver.py
@@ -5,22 +5,24 @@ i=0
 while i < len(equation):
       if i<len(equation) and (equation[i]=="*" or equation[i]=="/"):
             if equation[i]=="*":
-                  equation[i]=(equation[i-1]*(i+1))
+                  equation[i]=(int(equation[i-1])*int(equation[i+1] ))
             else:
-                  equation[i]=(equation[i-1] /(i+1))
+                  equation[i]=(int(equation[i-1]) /int(equation[i+1]))
             equation.remove(equation[i-1])
             equation.remove(equation[i])
       i =i+1
+i=0
 while i < len(equation):
       if i < len(equation) and (equation[i]=="+" or equation[i]=="-"):
             if equation[i]=="+":
-                  equation[i]=(equation[i-1] + (i+1))
+                  equation[i]=(int(equation[i-1]) + int(equation[i+1]))
             else:
-                  equation[i]=(equation[i-1]-(i+1))
+                  equation[i]=(int(equation[i-1])-int(equation[i+1]))
             equation.remove(equation[i-1])
             equation.remove(equation[i])
       i =i+1
 print(equation)
+
 
 
       
